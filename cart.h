65e4dc5a5f44993d4283da5131115e7b526d79c0
Amar_Asfaw <amarasfaw1077@gmail.com>
2025-12-24 19:08:57 +0300

    Add shopping cart data structure

---
+++ cart.h
@@ -0,0 +1,14 @@
+#ifndef CART_H
+#define CART_H
+
+struct CartNode {
+    int productID;
+    int quantity;
+    CartNode* next;
+};
+
+void addToCart(int id, int qty);
+void viewCart();
+float generateBill();
+
+#endif
